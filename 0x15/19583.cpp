c9ba608f770474471ec6434fad371119b0c25afd
myeowon <myw7931@gmail.com>
2024-08-14 00:24:15 +0900

    0x15 해시 : 과제

--- 0x15/19583.cpp
+++ 0x15/19583.cpp
@@ -4,30 +4,10 @@
 using namespace std;
 unordered_set<string> m;
 
-pair<int, int> startInt;
-pair<int, int> endInt;
-pair<int, int> closeInt;
-pair<int, int> toInt(string str)
+int toInt(string str)
 {
-    str.erase(remove(str.begin(), str.end(), ':'), str.end());
-    int strInt = stoi(str);
-    return {strInt / 100, strInt % 100};
-}
-
-bool beforeStart(pair<int, int> timeInt)
-{
-    if (timeInt.first < startInt.first)
-        return true;
-    else if (timeInt.first == startInt.first && timeInt.second <= startInt.second)
-        return true;
-    return false;
-}
-bool afterEnd_BeforeClose(pair<int, int> timeInt)
-{
-    if (timeInt.first > endInt.first || (timeInt.first == endInt.first && timeInt.second >= endInt.second))
-        if (timeInt.first < closeInt.first || (timeInt.first == closeInt.first && timeInt.second <= closeInt.second))
-            return true;
-    return false;
+    int strInt = 60 * stoi(str.substr(0, 2)) + stoi(str.substr(3, 2));
+    return strInt;
 }
 int main()
 {
@@ -36,26 +16,23 @@ int main()
 
     string start, end, close;
     cin >> start >> end >> close;
-    startInt = toInt(start);
-    endInt = toInt(end);
-    closeInt = toInt(close);
+
+    int startInt = toInt(start);
+    int endInt = toInt(end);
+    int closeInt = toInt(close);
+
     int result = 0;
-    while (true)
+    string time, name;
+
+    while (cin >> time >> name)
     {
-        string time;
-        cin >> time;
-        pair<int, int> timeInt = toInt(time);
+        int timeInt = toInt(time);
 
-        string name;
-        cin >> name;
-        if (cin.eof() == true)
-            break;
-
-        if (beforeStart(timeInt))
+        if (timeInt <= startInt)
         {
             m.insert(name);
         }
-        else if (afterEnd_BeforeClose(timeInt))
+        else if (timeInt >= endInt && timeInt <= closeInt)
         {
             if (m.find(name) != m.end())
             {
@@ -65,4 +42,5 @@ int main()
         }
     }
     cout << result;
+    return 0;
 }
